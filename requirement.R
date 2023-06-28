82a884911eb9591564e68ff88e25d6ac6f9e806e
ShubhamMisar-12 <shubham007me@gmail.com>
2023-06-28 10:12:09 -0400

    Cleaned Repo

--- requirement.R
+++ requirement.R
@@ -1,4 +1,25 @@
-packages <- readLines("requirements.txt")
+packages <- c(
+  "tsibble",
+  "dplyr",
+  "ggplot2",
+  "fabletools",
+  "feasts",
+  "tsibbledata",
+  "forecast",
+  "tidyverse",
+  "cowplot",
+  "rugarch",
+  "zoo",
+  "lubridate",
+  "scales",
+  "stringr",
+  "tibble",
+  "magrittr",
+  "tidyr",
+  "readr",
+  "anytime",
+  "here"
+)
 
 # Install each package
 for (pkg in packages) {
